5e2c5df350803c7f223530775b40c84c4c20f648
PxChainWarden <45844905+PxChainWarden@users.noreply.github.com>
2021-08-18 17:03:49 +0300

    API Update
    Added get/create/update/delete role API calls.

--- include/api.tickets.php
+++ include/api.tickets.php
@@ -553,6 +553,119 @@ class TicketApiController extends ApiController {
         $this->response(200, json_encode($threadEntry));
     }
 
+    function getRole($format) {
+
+        if(!($key=$this->requireApiKey()) || !$key->canCreateTickets())
+            return $this->exerr(401, __('API key not authorized'));
+
+        $role = null;
+        if(!strcasecmp($format, 'email')) {
+            # Handle remote piped emails - could be a reply...etc.
+            $role = $this->processEmail();
+        } else {
+            $data = $this->getRequest($format);
+            if(isset($data['topic_id']))
+                $role = Role::lookup($data['role_id']);
+            else{
+                $role = $this->_getRole($data);
+            }
+        }
+
+        if(!$role)
+            return $this->exerr(500, __("Unable to find role: unknown error"));
+
+        $this->response(200, json_encode($role));
+    }
+
+    function createRole($format) {
+
+        if(!($key=$this->requireApiKey()) || !$key->canCreateTickets())
+            return $this->exerr(401, __('API key not authorized'));
+
+        $role = null;
+        if(!strcasecmp($format, 'email')) {
+            # Handle remote piped emails - could be a reply...etc.
+            $role = $this->processEmail();
+        } else {
+            $data = $this->getRequest($format);
+            $role = Role::create();
+            $errors = array();
+            $role->update($data,$errors);
+            if (count($errors)) {
+                if(isset($errors['errno']) && $errors['errno'] == 403)
+                    return $this->exerr(403, __('Role denied'));
+                else
+                    return $this->exerr(
+                            400,
+                            __("Unable to create new role: validation errors").":\n"
+                            .Format::array_implode(": ", "\n", $errors)
+                            );
+            } 
+        }
+        if(!$role)
+            return $this->exerr(500, __("Unable to find role: unknown error"));
+
+        $this->response(200, json_encode($role));
+    }
+
+    function updateRole($format) {
+
+        if(!($key=$this->requireApiKey()) || !$key->canCreateTickets())
+            return $this->exerr(401, __('API key not authorized'));
+
+        $role = null;
+        if(!strcasecmp($format, 'email')) {
+            # Handle remote piped emails - could be a reply...etc.
+            $role = $this->processEmail();
+        } else {
+            $data = $this->getRequest($format);
+            if(isset($data['role_id']))
+                $role = Role::lookup($data['role_id']);
+            else
+                return $this->exerr(400, __('No role_id provided: bad request body'));
+            $errors = array();
+            $role->update($data,$errors);
+            if (count($errors)) {
+                if(isset($errors['errno']) && $errors['errno'] == 403)
+                    return $this->exerr(403, __('Role denied'));
+                else
+                    return $this->exerr(
+                            400,
+                            __("Unable to create new role: validation errors").":\n"
+                            .Format::array_implode(": ", "\n", $errors)
+                            );
+            } 
+        }
+        if(!$role)
+            return $this->exerr(500, __("Unable to find role: unknown error"));
+
+        $this->response(200, json_encode($role));
+    }
+
+    function deleteRole($format) {
+
+        if(!($key=$this->requireApiKey()) || !$key->canCreateTickets())
+            return $this->exerr(401, __('API key not authorized'));
+
+        $role = null;
+        if(!strcasecmp($format, 'email')) {
+            # Handle remote piped emails - could be a reply...etc.
+            $role = $this->processEmail();
+        } else {
+            $data = $this->getRequest($format);
+            if(isset($data['role_id']))
+                $role = Role::lookup($data['role_id']);
+            else
+                return $this->exerr(400, __('No role_id provided: bad request body'));
+            $isDeleted = $role->delete();
+            
+        }
+        if(!$isDeleted)
+            return $this->exerr(500, __("Unable to delete role: unknown error"));
+
+        $this->response(200, json_encode($isDeleted));
+    }
+
     
 
     /* private helper functions */
@@ -881,6 +994,42 @@ class TicketApiController extends ApiController {
 
         // Clearing up
         $result = array('total'=>count($topics),'result'=>$topics);
+        return $result;
+    }
+
+    function _getRole($data){
+        // Init =================================================
+        // Declaring variables that we use.
+        $roles = array();        // roles array
+        $pageNumber = 1;        // Result page number
+        $limit = 25;            // Page ticket count limit
+        $criteria = null;       // Search criteria
+        
+        // Check Params =========================================
+        // Set page number if given (Default: 1)
+        if(isset($data['page']))
+            $pageNumber = $data['page'];
+        // Set roles per page limit if given (Default: 25)
+        if(isset($data['limit'])){
+            // Check if limit exceeds max limit
+            if((int)$data['limit'] < 100)
+                $limit = $data['limit'];
+            else
+                return $this->exerr(400, __("Limit can not exceed 100: bad request body")); 
+        }
+
+        // Create pagination for query
+        $pagination = new Pagenate(Role::objects()->count(), $pageNumber, $limit);
+        $query = Role::objects()->filter(array("flags" => true))->limit($pagination->getLimit())->offset($pagination->getStart());
+        //$page = $pagination->paginateSimple($query);
+        
+        // Get role information from the page and push it into roles array
+        foreach($query as $role){
+            array_push($roles,$role);
+        }
+
+        // Clearing up
+        $result = array('total'=>count($roles),'result'=>$roles);
         return $result;
     }
 
